bb983abc6c89dc6f722ba716ea5fd58466014390
abdul-local <abdullah.hamzan@gmail.com>
2020-07-25 13:59:45 +0800

    bab 25 sampai 26 selesai

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,53 @@
-// const mongoose= require('mongoose');
-// const schema=mongoose.schema;
-// const Postschema= new Schema({
+const mongoose= require('mongoose');
+const schema =mongoose.Schema;
+const Postschema = new schema({
+    // user reference
+    user:{
+        type:schema.Types.ObjectId,
+        ref:'users'
+    },
+    text:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String
+    },
+    likes:[
+        {
+            //relation/reference
+            user:{
+                type: schema.Types.ObjectId,
+                ref:'users'
+            }
+        }
+    ],
+    comments:[
+        {
+            user:{
+                type:schema.Types.ObjectId,
+                ref:'users'
+            },
+            text:{
+                type:String,
+                required:true
+            },
+            name:{
+                type:String
+            },
+            avatar:{
+                type:String
+            },
+            date:{
+                type:Date,
+                default:Date.now
+            }
+        }
+    ],
+    date:{
+        type:Date,
+        default:Date.now
+    }
     
-// })
+});
+module.exports=Post=mongoose.model('post',Postschema);
