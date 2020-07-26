2cb18f8470c1c8dfb90193448d9ce4e11a818b01
abdul-local <abdullah.hamzan@gmail.com>
2020-07-27 00:30:29 +0800

    Auth register dan register action sudah selesai

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,9 +2,10 @@ import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setAlert }  from '../../actions/alert';
+import { register }  from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert , register}) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -23,7 +24,8 @@ const Register = ({ setAlert }) => {
       //console.log('Password do not match');
       setAlert('password do not match','danger');
     } else {
-      console.log(formData);
+     // console.log(formData);
+     register({ name, email, password})
     }
   };
 
@@ -40,7 +42,7 @@ const Register = ({ setAlert }) => {
               name="name"
               value={name}
               onChange={e => onChange(e)}
-              required
+    
             />
           </div>
           <div className="form-group">
@@ -50,7 +52,7 @@ const Register = ({ setAlert }) => {
               name="email"
               value={email}
               onChange={e => onChange(e)}
-              required
+              
             />
           </div>
           <div className="form-group">
@@ -83,6 +85,7 @@ const Register = ({ setAlert }) => {
   )
 }
 Register.propTypes ={
-    setAlert: PropTypes.func.isRequired
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired
 }
-export default connect(null, { setAlert }) (Register);
+export default connect(null, { setAlert , register}) (Register);
--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,5 +1,5 @@
 import { SET_ALERT,REMOVE_ALERT } from '../actions/types';
-import uuid from 'uuid';
+import { v4 as uuid} from 'uuid';
 const initialState = [];
 
 export default function(state = initialState, action){
@@ -7,7 +7,7 @@ export default function(state = initialState, action){
 
     switch (type) {
         case SET_ALERT:
-            return [...state, action.payload];
+            return [...state, payload];
         case REMOVE_ALERT:
             return state.filter(alert => alert.id !== action.payload);
         default:
