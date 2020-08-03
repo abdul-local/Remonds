e434239c902eec1ee4329ae99f16564e42ef57d6
abdul-local <abdullah.hamzan@gmail.com>
2020-08-03 11:42:38 +0800

    like dan unlike sudah selesai

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import {
     GET_POSTS,
-    POST_ERROR
+    POST_ERROR,
+    UPDATE_LIKES
 } from './types';
 
 // Get posts
@@ -22,3 +23,40 @@ export const getPosts = () => async dispatch => {
         })
     }
 }
+// Add Like
+export const addLike= id => async dispatch => {
+    try{
+        const res= await axios.put(`/api/posts/like/${id}`);
+        dispatch({
+            type:UPDATE_LIKES,
+            payload: {id, likes: res.data }
+        })
+    }catch(err){
+        dispatch({
+
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status}
+        });
+
+        
+    }
+};
+
+// Remove Like
+export const removeLike= id => async dispatch => {
+    try{
+        const res = await axios.put(`/api/posts/unlike/${id}`);
+        dispatch({
+            type:UPDATE_LIKES,
+            payload: {id, likes: res.data }
+        })
+    }catch(err){
+        dispatch({
+
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status}
+        });
+
+        
+    }
+};
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,10 +1,13 @@
-import React, { Fragment } from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
-import Moment from 'react-moment'
-import { connect } from 'react-redux'
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
+import { connect } from 'react-redux';
+import { addLike, removeLike } from '../../actions/post';
 
 const PostItem = ({ 
+    addLike,
+    removeLike,
     auth,
     deletePost,
     post: { _id, text, name, avatar, user, likes, comments, date }
@@ -24,11 +27,19 @@ const PostItem = ({
             <p class="post-date">
                 Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
             </p>
-            <button type="button" class="btn btn-light">
+
+            <button 
+                onClick={e => addLike(_id)}
+                type="button"
+                class="btn btn-light">
                 <i class="fas fa-thumbs-up"></i>
                 <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
             </button>
-            <button type="button" class="btn btn-light">
+
+            <button 
+                onClick={e => removeLike(_id)}
+                type="button" 
+                class="btn btn-light">
                 <i class="fas fa-thumbs-down"></i>
             </button>
             <Link to={`/posts/${_id}`} class="btn btn-primary">
@@ -51,11 +62,14 @@ const PostItem = ({
 
 PostItem.propTypes = {
     post: PropTypes.object.isRequired,
-    auth: PropTypes.object.isRequired
+    auth: PropTypes.object.isRequired,
+    addLike:PropTypes.func.isRequired,
+    removeLike:PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
-    auth: state.auth
+    auth: state.auth,
+    
 });
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, {addLike, removeLike})(PostItem);
